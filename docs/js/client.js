86ffd0dfb9ec435604fd6fabf8d1d157e0c32112
Juan Franco López <juan.franco.lopez@hotmail.com>
2026-01-31 16:21:50 +0100

    Cambios varios.

--- docs/js/client.js
+++ docs/js/client.js
@@ -1,6 +1,6 @@
 // Script para mostrar la fecha actual en el div con id 'fecha'
 window.addEventListener('DOMContentLoaded', function() {
-  const fechaDiv = document.getElementById('fecha');
+  const fechaDiv = document.getElementById('date');
   const fechaActual = new Date();
   // Formato: día/mes/año
   const fechaFormateada = fechaActual.toLocaleDateString();
